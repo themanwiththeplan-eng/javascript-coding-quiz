0cf3c0e846771224e44c182c678530a2339017f0
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-16 23:02:56 -0800

    fixed some logic issues, made more well rounded functions, still have to fix the logic in the if else statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,12 @@ var rightWrong = document.querySelector('.right')
 var questionAsked = document.querySelector('.question-asked')
 var count = 60;
 var questionArr = ["What data types are Dictionaries considered in Javascript?", "What is the correct declaration of a for loop?", "What is an array in javascript?", "What is the correct way to get an element by id in javascript?", "How do you set an item in localStorage using javascript?"]
-var answerOne = ["Object", "String",  "Array",  "Integer"];
-var answerTwo = ["for(var i = 0 i < 1 i--){}", "for(var i=1, i < 2, i++{}", "for(var i = 0; i < 0; i++){};", "for( var i<1 i=2 i--);"]
+var answers = [{1: "Object", 2: "String", 3: 'Array', 4: "Integer"}, {1: "for(i<1. i=2){};", 2: "for(s in students; s=0){};", 3: "for(var i = 0; i < 0; i++){}", 4: "for([key][value] of Object.entries('object';)){};"}];
 startEl.textContent = `Start Quiz`;
 startEl.addEventListener('click', startQuiz);
 var score = '';
 var questionAsk = 0;
-
+var index = 0;
 function startQuiz(){
 
     var timerDecrement = setInterval(function(){
@@ -34,23 +33,41 @@ function startQuiz(){
 
     document.getElementById('questions').style.display = "flex";
     questionAsked.textContent = `${questionArr[questionAsk]}`;
-    oneEl.innerHTML = answerOne[0];
-    twoEl.innerHTML = answerOne[1];
-    threeEl.innerHTML = answerOne[2];
-    fourEl.innerHTML = answerOne[3];
-
+    oneEl.innerHTML = answers[0][1];
+    twoEl.innerHTML = answers[0][2];
+    threeEl.innerHTML = answers[0][3];
+    fourEl.innerHTML = answers[0][4];
 }
-oneEl.addEventListener('click',function(){
-    score += 5
-    questionAsked.textContent = questionArr[1];
-    oneEl.innerHTML = answerTwo[0];
-    twoEl.innerHTML = answerTwo[1];
-    threeEl.innerHTML = answerTwo[2];
-    fourEl.innerHTML = answerTwo[3];
-    rightWrong.textContent = "Right Good Job";
-    console.log(score);
-});
+// oneEl.addEventListener('click',rightAnswerOne);
+// twoEl.addEventListener('click', wrongAnswerOne);
+// threeEl.addEventListener('click', wrongAnswerOne);
+// fourEl.addEventListener('click', wrongAnswerOne);
+oneEl.addEventListener('click', rightWrongAnswer);
+twoEl.addEventListener('click', rightWrongAnswer);
+threeEl.addEventListener('click', rightWrongAnswer);
+fourEl.addEventListener('click', rightWrongAnswer);
 
-function wrongAnswerOne(){
-    questionAsked.textContent = questionArr[questionAsk++];
+function rightWrongAnswer(){
+    if(answers[0][1]){
+        score += 5;
+        questionAsked.textContent = questionArr[questionAsk += 1];
+        index++;
+        oneEl.innerHTML = answers[index][1];
+        twoEl.innerHTML = answers[index][2];
+        threeEl.innerHTML = answers[index][3];
+        fourEl.innerHTML = answers[index][4];
+        console.log(score);
+        rightWrong.textContent = `Congrats you got it right!`
+    }
+    else{
+        count -= 5;
+        questionAsked.textContent = questionArr[questionAsk += 1];
+        index++;
+        oneEl.innerHTML = answers[index][1];
+        twoEl.innerHTML = answers[index][2];
+        threeEl.innerHTML = answers[index][3];
+        fourEl.innerHTML = answers[index][4];
+        console.log(score);
+        rightWrong.textContent = `Sorry, you got it wrong!`
+    }
 }
