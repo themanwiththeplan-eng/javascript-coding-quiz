6dc8fe54748f9dd78c57290c80acc328549f973b
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-17 18:54:46 -0800

    fixed logic for functions involving the buttons, made functionality for view highscore button as well as the go back button also made the clearHighscores function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+var highestScores = document.querySelector('.highest-scores');
+var emptyHigh = {}
+var goBackBtn = document.querySelector('.goBack');
+var clearHighscores = document.querySelector('.clear');
+var highscores = document.querySelector('.highscores');
 var oneEl = document.querySelector('.answerOne');
 var twoEl = document.querySelector('.answerTwo');
 var threeEl = document.querySelector('.answerThree');
@@ -8,26 +13,31 @@ var introEl = document.querySelector('.quiz-intro');
 var rightWrong = document.querySelector('.right')
 var questionAsked = document.querySelector('.question-asked')
 var count = 60;
-var questionArr = ["What data types are Dictionaries considered in Javascript?", "What is the correct declaration of a for loop?", "What is an array in javascript?", "What is the correct way to get an element by id in javascript?", "How do you set an item in localStorage using javascript?"]
-var answers = [{1: "Object", 2: "String", 3: 'Array', 4: "Integer"}, {1: "for(i<1. i=2){};", 2: "for(s in students; s=0){};", 3: "for(var i = 0; i < 0; i++){}", 4: "for([key][value] of Object.entries('object';)){};"}];
+var questionArr = ["What data types are Dictionaries considered in Javascript?", "What is the correct declaration of a for loop?", "How is an array declared in javascript?", "What is the correct way to get an element by id in javascript?", "How do you set an item in localStorage using javascript?"]
+var answers = [{1: "Object", 2: "String", 3: 'Array', 4: "Integer"}, {1: "for(i<1. i=2){};", 2: "for(s in students; s=0){};", 3: "for(var i = 0; i < 0; i++){}", 4: "for([key][value] of Object.entries('object';)){};"}, {1: `var x = {};`, 2: `var x = ();`, 3: `var x == [];`, 4: `var x = [];`}, {1: 'document.getElementById', 2: 'document.location.reload', 3: 'document.querySelector', 4: 'localStorage.setItem'}, {1: 'document.querySelector', 2: 'document.getElementById', 3: 'localStorage.setItem', 4: 'object.toString'},{}];
 startEl.textContent = `Start Quiz`;
 startEl.addEventListener('click', startQuiz);
-var score = '';
+var score = 0;
 var questionAsk = 0;
 var index = 0;
-function startQuiz(){
 
+function startQuiz(){
+   
+    
+    
+    
     var timerDecrement = setInterval(function(){
     
-        
+    
         if(count > 0){
             count--;
-            timerEl.textContent = `Timer: ${count}`
+            timerEl.textContent = `Timer: ${count}`;
         }else{
-            timerEl.textContent = `Quiz over your score was ${score}`;
             clearInterval(timerDecrement);
+            timerEl.textContent = `Your quiz is over, your score was ${score}`;
         }
     }, 1000)
+    
     document.getElementById('quiz').style.display = "none";
     document.getElementById('starter').style.display = "none";
 
@@ -37,37 +47,101 @@ function startQuiz(){
     twoEl.innerHTML = answers[0][2];
     threeEl.innerHTML = answers[0][3];
     fourEl.innerHTML = answers[0][4];
-}
-// oneEl.addEventListener('click',rightAnswerOne);
-// twoEl.addEventListener('click', wrongAnswerOne);
-// threeEl.addEventListener('click', wrongAnswerOne);
-// fourEl.addEventListener('click', wrongAnswerOne);
-oneEl.addEventListener('click', rightWrongAnswer);
-twoEl.addEventListener('click', rightWrongAnswer);
-threeEl.addEventListener('click', rightWrongAnswer);
-fourEl.addEventListener('click', rightWrongAnswer);
 
-function rightWrongAnswer(){
-    if(answers[0][1]){
-        score += 5;
-        questionAsked.textContent = questionArr[questionAsk += 1];
-        index++;
-        oneEl.innerHTML = answers[index][1];
-        twoEl.innerHTML = answers[index][2];
-        threeEl.innerHTML = answers[index][3];
-        fourEl.innerHTML = answers[index][4];
-        console.log(score);
-        rightWrong.textContent = `Congrats you got it right!`
+    highscores.addEventListener('click', function(){
+        clearInterval(timerDecrement);
+        timerEl.textContent = ``;
+    })
+}
+
+oneEl.addEventListener('click',function(){
+    if(questionAsk == 0 || questionAsk == 3){
+        rightAnswer();
+    }else{
+        wrongAnswer();
     }
-    else{
-        count -= 5;
-        questionAsked.textContent = questionArr[questionAsk += 1];
-        index++;
-        oneEl.innerHTML = answers[index][1];
-        twoEl.innerHTML = answers[index][2];
-        threeEl.innerHTML = answers[index][3];
-        fourEl.innerHTML = answers[index][4];
-        console.log(score);
-        rightWrong.textContent = `Sorry, you got it wrong!`
+    if(questionAsk > 4){
+        saveScore();
     }
+})
+twoEl.addEventListener('click', function(){
+    wrongAnswer();
+    if(questionAsk > 4){
+        saveScore();
+    }
+})
+threeEl.addEventListener('click', function(){
+    if(questionAsk == 1 || questionAsk == 4){
+        rightAnswer();
+    }else{
+        wrongAnswer();
+    }
+    if(questionAsk > 4){
+        saveScore();
+    }
+})
+fourEl.addEventListener('click', function(){
+    if(questionAsk == 2){
+        rightAnswer();
+    }else{
+        wrongAnswer();
+    }
+    if(questionAsk > 4){
+        saveScore();
+    }
+})
+
+highscores.addEventListener('click', viewHighscore);
+
+
+function viewHighscore(){
+    
+    document.getElementById('quiz').style.display = "none";
+    document.getElementById('starter').style.display = "none";
+    document.getElementById('questions').style.display = "none";
+
+    document.getElementById('highscoreForm').style.display = "flex";
+    highestScores.textContent = emptyHigh;
+}
+
+goBackBtn.addEventListener('click', goBack);
+function goBack(){
+    document.getElementById('quiz').style.display = "flex";
+    document.getElementById('starter').style.display = "flex";
+    
+    document.getElementById('highscoreForm').style.display = "none";
+}
+function clearHighscores(){
+    localStorage.clear();
+}
+function saveScore(){
+    var id = prompt(`What are your initials?`)
+    emptyHigh.assign(id, score);
+    highestScores.textContent = emptyHigh;
+    localStorage.setItem('Highscores', JSON.stringify(emptyHigh));
+}
+function rightAnswer(){
+    score = score + 5;
+    questionAsked.textContent = questionArr[++questionAsk];
+    index++;
+    oneEl.innerHTML = answers[index][1];
+    twoEl.innerHTML = answers[index][2];
+    threeEl.innerHTML = answers[index][3];
+    fourEl.innerHTML = answers[index][4];
+    console.log(score);
+    rightWrong.textContent = `Congrats you got it right! Youre score is now ${score}`;
+}
+function wrongAnswer(){
+    count -= 5;
+    questionAsked.textContent = questionArr[++questionAsk];
+    index++;
+    oneEl.innerHTML = answers[index][1];
+    twoEl.innerHTML = answers[index][2];
+    threeEl.innerHTML = answers[index][3];
+    fourEl.innerHTML = answers[index][4];
+    console.log(score);
+    rightWrong.textContent = `Sorry, you got it wrong!`;
+}
+if(questionAsk > 4){
+    saveScore();
 }
